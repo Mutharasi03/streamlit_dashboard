503fa52d61cab6ca2828e768bcd9f9cf226b3fac
Mutharasi k <88454070+Mutharasi03@users.noreply.github.com>
2024-03-06 01:58:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -373,6 +373,47 @@ if selected == "model":
 
         label_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
 
+    from sklearn.preprocessing import LabelEncoder
+    LE = LabelEncoder()
+    df1=df1.apply(LE.fit_transform)
+    st.write(df1)
+
+    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.metrics import classification_report, accuracy_score
+
+
+    # Assuming 'target_column' is your target variable
+    X = df1 # Features
+    y = df['Accident_severity']  # Target variable
+
+    # Perform train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # 2. Model Training
+    # Initialize Random Forest Classifier
+    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    # Fit the model to the training data
+    rf_classifier.fit(X_train, y_train)
+
+    # 3. Model Evaluation
+    # Predict on the testing set
+    y_pred = rf_classifier.predict(X_test)
+
+    # Evaluate the model
+    st.write("Accuracy:", accuracy_score(y_test, y_pred))
+    # st.write("Classification Report:")
+    # st.table(classification_report(y_test, y_pred))
+
+    report = classification_report(y_test, y_pred, output_dict=True)
+    rep = pd.DataFrame(report).transpose()
+
+    # Display the classification report as a table
+    st.write("Classification Report:")
+    st.table(rep)
+    
+
         st.subheader('Label mapping:')
         st.write(label_mapping)
 
